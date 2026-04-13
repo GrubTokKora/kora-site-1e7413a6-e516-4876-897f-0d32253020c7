18be8f5c080773682ed3caaf9aaad7e4004ad5d2
kysz-harsh <harsh.prajapati@kysz.tech>
2026-04-13 23:26:03 +0530

    Add address and Google Maps link to top bar; enhance CSS for address link styling

--- components.js
+++ components.js
@@ -4,6 +4,8 @@ const UBEREATS_URL = "https://www.ubereats.com/store/tabla-indian-cuisine/idH05N
 const TOAST_URL = "https://order.toasttab.com/online/tabla-38245-w-10-mile-rd?diningOption=takeout&rwg_token=AJKvS9XH-1E7e6IHM9ULqvAFi1CPc2z5CSVW5KKXBs9JovYGp-uL6OmmyFe4-Jib-EXMSx4NTZmR6baBYHzwuT6WM8vhbdTRqy84oMEuJjrDiqxVRpIRlhU%3D";
 const DOORDASH_URL = "https://www.doordash.com/store/tabla-llc-farmington-hills-29708756/?utm_campaign=gpa";
 const GRUBHUB_URL = "http://menus.fyi/8977496";
+const ADDRESS_LINE = "38245, 38249 W 10 Mile Rd, Farmington Hills, MI 48335";
+const GOOGLE_MAPS_URL = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(ADDRESS_LINE)}`;
 
 function renderHeader(activePage = 'home') {
   const pages = [
@@ -29,7 +31,7 @@ function renderHeader(activePage = 'home') {
     <div class="top-bar">
       <div class="top-bar-inner">
         <div class="top-bar-left">
-          <span class="top-bar-item"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 16 16"><path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/></svg> 38245, 38249 W 10 Mile Rd, Farmington Hills, MI 48335</span>
+          <a class="top-bar-item top-bar-address-link" href="${GOOGLE_MAPS_URL}" target="_blank" rel="noopener noreferrer" aria-label="Open address in Google Maps"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true"><path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/></svg> ${ADDRESS_LINE}</a>
           <span class="top-bar-item"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/></svg> <a href="tel:248-536-2650">248-536-2650</a></span>
           <span class="top-bar-item"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 16 16"><path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/></svg> <a href="mailto:tabladetroit@gmail.com">tabladetroit@gmail.com</a></span>
         </div>
